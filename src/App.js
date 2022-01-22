7eb99f5cea77a826f7c436bc2abeabfc69c2c4e1
Jundi <indana6969@gmail.com>
2022-01-22 14:48:45 +0700

    fix the file structure and adding some components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
+// components
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
-      GPT-3
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
